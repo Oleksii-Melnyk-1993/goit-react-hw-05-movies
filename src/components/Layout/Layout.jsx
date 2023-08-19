e5d7a57658c07a1dbbf23d5f595f2db7f08fe699
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-08-19 16:04:03 +0300

    add  Toaster component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,21 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import { Container, Header, Navigation, StyledNavLink } from './Layout.styled';
 
 export const Layout = () => {
   return (
     <Container>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        containerStyle={{
+          top: 5,
+          left: 20,
+          bottom: 20,
+          right: 20,
+        }}
+      />
       <Header>
         <Navigation>
           <StyledNavLink to="/">Home</StyledNavLink>
